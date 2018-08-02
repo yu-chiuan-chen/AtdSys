f1d7d9e72585b3ef6cffb7d6eec0e94cd1cd16f7
Hans Hsieh <dinghanx@gmail.com>
2018-08-02 19:07:42 +0800

    Hans: 加入struts2，二版。

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,27 +1,14 @@
 package demo;
 
 public class Demo {
-	// Now you see me!!!!!!??
-	// SUCCESS!???
 	
-	public static void printHello(String name){
-		System.out.println("Hello11111 "+name);
-		System.out.println("Hello12332112345678 "+name);
-	}
-	
-	public static void printHello2(String name){
-		System.out.println("Hello "+name);
-		System.out.println("Hello Hello Hello"+name);
-	}
-	
+	// 單機測試用
 	public static void main(String[] args) {
 		System.out.println("Hello world!!");
-		Demo.printHello("Amy!");
-		
 		// test Spring
-		SpringDemo.getInstance().testSpring();
+		//SpringDemo.getInstance().testSpring();
 		// test Hibernate
-//		HibernateDemo.getInstance().testHibernate();
+		//HibernateDemo.getInstance().testHibernate();
 	}
 	
 }
